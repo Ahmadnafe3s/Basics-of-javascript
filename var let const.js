8224b436ec574e58946f22f9b785a51484c49345
Nafees Ahmad <nafees56752@gmail.com>
2023-01-29 13:21:03 +0530

    difference between let var const

--- var let const.js
+++ var let const.js
@@ -1,8 +1,25 @@
 var a = "nafees";
 
-// {
-//     var a = "faizan"
-//     console.log(a);
-// }
+{
+    var a = "faizan"
+    console.log(a); // out put will be faizan faizan
+}
 
 console.log(a);
+
+let b = "zeeshan";
+
+{
+    let b = "nafees"
+    console.log(b); // Output will be nafees zeeshan
+}
+
+console.log(b);
+
+const num = 100
+
+// let num = 30; // it thorws error
+
+console.log(num);
+
+const name = 0; // we can do this
