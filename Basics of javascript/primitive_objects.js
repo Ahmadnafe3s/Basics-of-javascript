3b0744fcc039f4e2754f712ea7a3968d60139887
Nafees Ahmad <nafees56752@gmail.com>
2023-01-29 14:18:10 +0530

    This is the basics of primitive and non primitive data types

---
+++ Basics of javascript/primitive_objects.js
@@ -0,0 +1,22 @@
+// NN BB SS U
+
+let a = null
+let b = 530
+let c = true // false
+let d = BigInt(890) + BigInt(200)
+let e = Symbol("you are good person") // primitive data types
+let f = "Faizan"
+let  g = undefined
+
+console.log(a , b , c , d , e , f , g);
+
+
+// OBjects 
+
+const item = {
+    "nafees" : true,
+    "Faizan" : false, // it also known as dictinory in the python but in JS it is objects
+    "Zeeshan" : 89
+}
+
+console.log(item["nafees"]);
