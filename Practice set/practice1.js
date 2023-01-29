4bef22a8a58e57b6512cb10fe32bbfe84cf8b8d5
Nafees Ahmad <nafees56752@gmail.com>
2023-01-29 19:43:31 +0530

    This the practice set 1

---
+++ Practice set/practice1.js
@@ -0,0 +1,24 @@
+// Ques 1
+
+let a = "Nafees" , b = 34
+
+console.log(a + b);
+
+// Ques 2
+
+console.log(typeof a , typeof b);
+
+
+
+
+// WAP to create dictinory of 5 words
+
+const dict = {
+going : "Jaana" ,
+come : "aana",     
+do : "karna",      // we cant assign any value into the dict variable coz it is a object. .... .
+seprate : "alag",
+sunny : "leonie"
+}
+
+console.log(dict['sunny']);
